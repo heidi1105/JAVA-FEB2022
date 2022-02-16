e76973e619a3b4adf4b0bd5a95544302b2b3a25f
heidi1105 <heidi1105@gmail.com>
2022-02-16 13:42:39 -0800

    complete Full CRUD project folder

---
+++ demos/w2d3/full-crud-project-folder/src/test/java/com/heidichen/firstmvcdemo/FirstMvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heidichen.firstmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMvcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
