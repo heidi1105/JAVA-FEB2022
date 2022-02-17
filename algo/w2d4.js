e47000e511fa84864a35c872d0ee5320d964c44e
heidi1105 <heidi1105@gmail.com>
2022-02-17 09:35:45 -0800

    algo update

--- algo/w2d4.js
+++ algo/w2d4.js
@@ -1,8 +1,9 @@
-class Node{
-	constructor(data){
-		this.data = data;
-		this.next = null;
-	}
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
 }
 
 class SLLStack {
@@ -22,6 +23,7 @@ class SLLStack {
 
     pop() {
         if (this.top === null) return null;
+
         var removed = this.top; 
         this.top = this.top.next; 
         removed.next = null; 
@@ -29,18 +31,33 @@ class SLLStack {
     }
 
     peek() {
-        return this.top.data;
+        return this.top? this.top.data: null;
+    }
+
+    isEmpty() {
+        return this.top === null;
+    }
+
+    getLength() {
+        return this.length;
     }
 
     printStack(){
-      // print every node in the stack (hint: by using push() and pop()
-      // make sure the original stack is back to the original state
-    }
+      //print the stack using only stack operations
+      // return the stack back to the original state
+      let tempStack = new SLLStack();
+      let temp;
+      console.log("top: " + this.peek())
+      while(!this.isEmpty()){
+        let temp = this.pop()
+        console.log(temp);
+        tempStack.push(temp);
+      }
 
-    copyStack(){
-      //create a second stack and copy values from the first stack into the second stack, so they are in the same order. 
+      while(!tempStack.isEmpty()){
+        this.push(tempStack.pop());
+      }
     }
-
 }
 
 
@@ -48,15 +65,56 @@ class SLLQueue{
 	constructor(){
 		this.front = null;
 		this.rear = null;
-
 	}
 
 
 	isPalindrome(){
-		//Determine if the queue is a palindrome (same items forward and backwards)
-		// Use only 1 stack as additional storage, no other arrays or objects
-    // tacocat
+		//Determine if the queue is a palindrome (same items forward and backwords)
+		// Use only stack and queue for additional storage
 	}
+
+	compareQueue(q2){
+		let isEqual = true;
+	    let newq1 = new SLLQueue()
+	    let newq2 = new SLLQueue()
+	    // q1: a,b,c,d.  q2: a,b,c 
+	    while(!this.isEmpty() || !q2.isEmpty()){
+	      let temp = this.dequeue();
+	      let temp2 = q2.dequeue();
+	      if(temp != temp2){
+	        console.log("false")
+	        isEqual = false;
+	      }
+	        newq1.enqueue(temp);
+	        newq2.enqueue(temp2); 
+	      }
+	     // the while loop will stop when one of the q ends
+
+	    // q1 is not empty but q2 is empty 
+	    if(!this.isEmpty() && q2.isEmpty()){
+	        isEqual = false;
+	      }
+	    if(this.isEmpty() && !q2.isEmpty()){
+	        isEqual = false;
+	      }
+
+	    
+	    while(!newq1.isEmpty() && !newq2.isEmpty()){
+	        let temp3 = newq1.dequeue()
+	        this.enqueue(temp3)
+	        let temp4 = newq2.dequeue()
+	        q2.enqueue(temp4)
+	      }
+	    if(isEqual){
+	      console.log("they are equal")
+	    }else{
+	      console.log("they are not equal")
+	    }
+		// compare this queue to another given queue to see if they are equal
+		// do not use any extra array or objects
+		// the queue should return to the original order when order
+	}
+
 
 
 	getFront(){
@@ -67,17 +125,7 @@ class SLLQueue{
 		}
 		//return the front
 	}
-
-	getRear(){
-		if(this.rear){
-			return this.rear.data;
-		}else{
-			return null;
-		}
-		//return the front
-	}
-
-  enqueue(data){
+    enqueue(data){
         // construct a new node and add to the current queue
         var node = new Node(data);
         if (this.front == null) {
@@ -113,28 +161,47 @@ class SLLQueue{
 		// check if the queue is empty
 	}
 
-  printQueue(){ 
-    console.log("Front: "+ this.getFront());
-    console.log("Rear: "+ this.getRear());
-    var tempQ = new SLLQueue();
-    while(!this.isEmpty()){
-      var tempData = this.dequeue();
-      tempQ.enqueue(tempData);
-      console.log(tempData);
-    }
-    while(!tempQ.isEmpty()){
-      var tempData = tempQ.dequeue();
-      this.enqueue(tempData);
+	printQueue(){
+		console.log("Front: " + this.getFront());
+	    let q2 = new SLLQueue();
+	    while(!this.isEmpty()){
+	      let temp = this.dequeue();
+	      console.log(temp);
+	      q2.enqueue(temp)
+	    }
+	    while(!q2.isEmpty()){
+	      this.enqueue(q2.dequeue())
     }
 
-  }
+		
+	}
 
+	contains(searchVal) {
+	    let runner = this.front;
+
+	    while (runner) {
+	      if (runner.searchVal === searchVal) return true;
+	      runner = runner.next;
+	    }
+	    return false;
+  	}
 }
 
 
-var q1 = new SLLQueue();
-q1.enqueue("a");
-q1.enqueue("b");
-q1.enqueue("c");
-q1.enqueue("d");
-q1.printQueue();
+var q = new SLLQueue();
+q.enqueue("a");
+q.enqueue("b");
+q.enqueue("b");
+q.enqueue("a");
+//q.printQueue();
+
+
+
+var s = new SLLStack();
+s.push("a");
+s.push("b");
+s.push("c");
+s.push("d");
+s.printStack();
+s.printStack();
+
